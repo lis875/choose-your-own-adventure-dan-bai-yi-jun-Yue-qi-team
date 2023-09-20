010dbc3daa75dffd6f68a3407dcca6a75c4bad65
Skye <46261958+Sherlenbon@users.noreply.github.com>
2023-09-20 15:38:43 -0500

    Update New Snow White.py

--- New Snow White.py
+++ New Snow White.py
@@ -1,6 +1,10 @@
+print("Now you are chosen to play the role of the queen in the new snow white. Which question you want to ask the magic mirror?")
+print("A.Who is the most beautiful woman in the world? B.Who is the richest man in the world? C.Who is the cleverest guy in the world?")
+A = ("Who is the most beautiful woman in the world?")
+B = ("Who is the richest man in the world?")
+C = ("Who is the cleverest guy in the world?")
 
-print("Now you are chosen to play the role of the queen in the new snow white. Which question you want to ask the magic mirror? ")
+question = input("I choose: ")
 
-if (question  == " ") or (person == " "):
-  print
+
   
