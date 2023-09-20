3ab738dcae06cd5ac9b952eb4627427be5356ac2
Skye <46261958+Sherlenbon@users.noreply.github.com>
2023-09-20 13:53:51 -0500

    Add files via upload

---
+++ New Snow White.py
@@ -0,0 +1,8 @@
+
+print("Now you are chosen to play the role of the queen in the new snow white.")
+
+question = input("which question you want to ask the magic mirror? ")
+
+if (question  == "1") or (person == " "):
+  print
+  
