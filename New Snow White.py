94cb21ffee34370400e200d761edfbf4cbfdcd23
Skye <46261958+Sherlenbon@users.noreply.github.com>
2023-09-20 16:07:54 -0500

    Update New Snow White.py

--- New Snow White.py
+++ New Snow White.py
@@ -1,10 +1,21 @@
+print("A long time ago, a queen of a certain country gave birth to a girl in winter.\n\
+Her skin was as pure as snow, her lips were as red as blood, and her hair was as black as ebony and as beautiful as snow white.")
+print(" ")
+print("Therefore, she was named Snow White.\n\
+Shortly after the queen gave birth to the princess, He passed away, and the king married a beautiful, proud, vicious and evil woman as the queen.")
+print(" ")
+print("At the same time, she also became Snow White's stepmother. \n\
+On this day, the queen received a magical mirror that could answer all the questions in the world.")
+      
 print("Now you are chosen to play the role of the queen in the new snow white. Which question you want to ask the magic mirror?")
-print("A.Who is the most beautiful woman in the world? B.Who is the richest man in the world? C.Who is the cleverest guy in the world?")
+print(" ")
+print("A.Who is the most beautiful woman in the world? \n\
+B.Who is the richest man in the world? \n\
+C.Who is the cleverest guy in the world?")
+
 A = ("Who is the most beautiful woman in the world?")
 B = ("Who is the richest man in the world?")
 C = ("Who is the cleverest guy in the world?")
 
 question = input("I choose: ")
-
-
   
